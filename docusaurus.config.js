7d2c21614e03bd610b868f63eaa6fc4977a4184c
Chivta <arvlas78@gmail.com>
2024-05-12 15:49:44 +0300

    Initial commit.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Super Site',
+    title: 'super Site',
     tagline: 'xxx',
     favicon: 'img/favicon.ico',
 
