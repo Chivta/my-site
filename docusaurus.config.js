8b8950b58a49aa739cf9c668e11728cc291bf6d7
Chivta <arvlas78@gmail.com>
2024-05-12 16:02:03 +0300

    Test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'super Site',
-    tagline: 'yay',
+    title: 'Docs',
+    tagline: 'Docs are cool',
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
