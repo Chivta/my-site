c6b2f76993d947be07e66d4014ca0b16f699d529
Chivta <arvlas78@gmail.com>
2024-05-12 15:51:08 +0300

    Test commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'super Site',
-    tagline: 'xxx',
+    tagline: 'yay',
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
